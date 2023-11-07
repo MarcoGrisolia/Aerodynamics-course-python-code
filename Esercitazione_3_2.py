0718e9035944b3ea209fe99fbb3588e17589a4a8
Marco Grisolia <116038537+MarcoGrisolia@users.noreply.github.com>
2023-11-07 15:46:25 +0100

    Update Esercitazione_3_2.py

--- Esercitazione_3_2.py
+++ Esercitazione_3_2.py
@@ -17,13 +17,13 @@ V = V1 + V2
 plt.streamplot(X1, Y1, U, V)
 
 
-X1 , Y1 , U1, V1 = f.simpleIrrotational_Vortex(1)
-X2 , Y2 , U2, V2 = f.simpleIrrotational_Vortex(-1, y0 = 1e-6)
+# X1 , Y1 , U1, V1 = f.simpleIrrotational_Vortex(1)
+# X2 , Y2 , U2, V2 = f.simpleIrrotational_Vortex(-1, y0 = 1e-6)
 
-U = U1 + U2
-V = V1 + V2
+# U = U1 + U2
+# V = V1 + V2
 
-plt.streamplot(X1, Y1, U, V)
+# plt.streamplot(X1, Y1, U, V)
 plt.axis('equal')
 plt.grid(True)
 
