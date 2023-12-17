99bbcce49dca7037f2483e4143c8a649ef3722c8
Marco Grisolia <116038537+MarcoGrisolia@users.noreply.github.com>
2023-12-17 17:15:42 +0100

    try solve for first stokes problem

--- 1st Stokes Problem.py
+++ 1st Stokes Problem.py
@@ -3,11 +3,11 @@ from scipy.integrate import odeint
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
-eta = np.linspace(0, 7, 100)
+eta = np.linspace(0, 1, 100)
 
 # Define the Blasius ODE system
 def blasius_eq(f, t):
-    return (f[1], f[2], -0.5 * f[0] * f[2])
+    return (f[1], f[2], -2 * f[0] * f[2])
 
 def blas0(x, ts):
     initial_conditions = np.array([0, 0, x[0]])  
@@ -18,9 +18,9 @@ def blas0(x, ts):
 
 value = fsolve(blas0, [0.1], args=(eta,))
 
-print(f'The correct initial condition for f(7.0) = 0 is {value[0]}')
+print(f'The correct initial condition for f(1.0) = 0 is {value[0]}')
 
-initial_conditions = [0, 0, value[0]]
+initial_conditions = [1, 1, value[0]]
 
 f = odeint(blasius_eq, initial_conditions, eta)
 
@@ -32,4 +32,4 @@ plt.title('Blasius Solution')
 plt.grid(True)
 plt.legend(["Psi", "u/U_inf", "Tau_wall"], loc="lower right")
 
-plt.savefig("output.png")
+plt.savefig("1st Stokes Problem.png")
